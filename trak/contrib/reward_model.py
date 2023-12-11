4b047f9335d51baa0da22eef6c519957ed6ff276
danielz <31590926+danielz02@users.noreply.github.com>
2023-12-11 12:26:58 -0500

    Fix get_out_to_loss_grad return dimension

--- trak/contrib/reward_model.py
+++ trak/contrib/reward_model.py
@@ -38,7 +38,7 @@ class RLHFRewardModelingOutput(AbstractModelOutput):
             model, (weights, buffers), args=(), kwargs=kw_inputs
         )
 
-        return reward_difference.sum()
+        return reward_difference.sum().unsqueeze(-1)
 
     def get_out_to_loss_grad(self, model, weights, buffers, batch: Iterable[Tensor]) -> Tensor:
         (input_ids_chosen, token_type_ids_chosen, attention_mask_chosen, input_ids_rejected, token_type_ids_rejected,
@@ -58,5 +58,5 @@ class RLHFRewardModelingOutput(AbstractModelOutput):
 
         out_to_loss_grad = -torch.sigmoid(-reward_difference)
 
-        return out_to_loss_grad.clone().detach().unsqueeze(-1)
+        return out_to_loss_grad.clone().detach()
 
